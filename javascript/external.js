9f89658c516a99ca058f578a4cb10c033df5e068
TMFrancis <bostontomf@gmail.com>
2021-09-17 11:24:22 -0400

    Completed Task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -7,7 +7,7 @@ const x = "web";
 const y = "mapping";
 const out = x + y;
 document.write(out);
-*/
+
 // A prompt box is used to prompt users to input a value before entering a page
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
@@ -17,3 +17,9 @@ sign = prompt();            // open the blank prompt window
 sign = window.prompt('Are you feeling lucky');  // open w prompt text
 sign = window.prompt('Are you feeling lucky', 'sure'); // open prompt w text
                                                        // and default value
+*/
+x = 22;
+const y = 33;
+document.writeln(x + y);
+document.writeln("<br>");
+document.writeln(x += 33);
