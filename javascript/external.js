5a2bd53dc8fbe299216f5f119bcba64a6f5b2aa7
TMFrancis <bostontomf@gmail.com>
2021-09-17 11:07:56 -0400

    Completed Task 2.

--- javascript/external.js
+++ javascript/external.js
@@ -1,1 +1,4 @@
-document.write("Where is my JS code?");
+// Task 1: document.write("Where is my JS code?");
+// Task 2: window.alert(5+6);
+document.write('<h1 style="color: #2c7bb6;">A heading</h1>');
+document.write('<p>A sentence.</p>');
