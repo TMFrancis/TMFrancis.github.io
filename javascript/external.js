5d9d3e5758c790c048c6fd790352dfb9aed5a1b6
TMFrancis <BostonTomF@gmail.com>
2021-09-26 15:24:02 -0400

    Filled content from web maps array into the web maps table. (Lab 4). Note: unresolved undefined var changed #webmap-reviews font-size to fit data.

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,15 @@
+var webmaps =
+  [
+    ["Strava Global Heat Map",
+    "https://www.strava.com/heatmap#8.98/-72.28045/42.31237/hot/all",
+    "Strava Global Heatmap is developed and maintained by Strava, Inc."
+    ],
+    ["Wandrer.earth",
+    "https:/wandrer.earth/",
+    "Wandrer.earth is developed and maintained by Craig Durkin."
+    ]
+  ];
+
 function welcome()
 {
 let a = "Please enter your name.";
@@ -13,12 +25,12 @@ return message
 function webmap_table()
 {
   document.write("<table width=100%>");
-  for (var row=0; row < 2; row++)
+  for (var row=0; row < webmaps.length; row++)
   {
     document.write("<tr>");
-    for (var column=0; column < 3; column++)
+    for (var column=0; column < webmaps[0].length; column++)
     {
-      document.write("<td>" + row + "," + column + "</td>");
+      document.write("<td>" + webmaps[row][column] + "</td>");
     }
     document.write("</tr>");
   }
