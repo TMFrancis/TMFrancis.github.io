3558d275e7fe02aaee48e974412e433e3b3213cd
TMFrancis <BostonTomF@gmail.com>
2021-12-19 10:33:28 -0500

    House cleaning: remove unused files

--- javascript/greeting.js
+++
@@ -1,3 +0,0 @@
-// Javascript to prompt visitor for name and write to header in HTML doc
-var vistorName = window.prompt('Please enter your name', 'Lucky Devil');
-document.writeln("<h1>Hello " + vistorName + "!</h1>");
